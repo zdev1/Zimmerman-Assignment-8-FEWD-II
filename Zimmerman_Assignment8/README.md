3dd1a200bc153c6ce8e740e10f590de83a13d14e
zdev1 <46204727+zdev1@users.noreply.github.com>
2019-04-25 19:38:33 -0700

    initial commit
    initial commit

---
+++ Zimmerman_Assignment8/README.md
@@ -0,0 +1,2 @@
+# Project-10
+fewd assignment 8
---
+++ Zimmerman_Assignment8/Zimmerman_Assignment8/bankaccount/js/script.js
@@ -0,0 +1,31 @@
+/*eslint-env browser*/
+
+function promptMe1(){
+    var userAdjective = prompt("Please enter your name");
+    alert (userAdjective);
+}
+
+function promptMe2(){
+    var userAdjective = prompt("Please enter an amount to deposit");
+    alert (userAdjective);
+}
+
+function promptMe3(){
+    var userAdjective = prompt("Please enter an amount to withdrawal");
+    alert (userAdjective);
+}
+
+function bankAccount (ownerName) {
+    var balance
+    var owner = "";
+}
+
+return bankAccount("withdrawalAmount"){
+    
+    
+}
+
+return deposit("depositAmount", "getBalance", "getOwnerName");
+
+
+
